bfdf54ee71cd3a3069cdef1787b82c63c4916ebd
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2026-09-23 02:54:23 +0000

    fix: 修正webhoook缩进

--- routes/webhooks.py
+++ routes/webhooks.py
@@ -1,6 +1,5 @@
 import asyncio
 from pathlib import Path
-from typing import Any
 
 from fastapi import APIRouter, Depends, Response
 from loguru import logger
@@ -12,30 +11,48 @@ from clients.sonarr_client import SonarrClient
 from clients.tmdb_client import TmdbClient
 from clients.tvdb_client import TvdbClient
 from core.config import get_settings
-from core.dependencies import (get_ai_client, get_media_clients,
-                               get_notification_service, get_qb_client,
-                               get_radarr_clients, get_server_by_token,
-                               get_sonarr_clients, get_task_queue,
-                               get_tmdb_client, get_tvdb_client)
-from models.emby_webhook import (EmbyPayload, LibraryDeletedEvent,
-                                 LibraryNewEvent)
+from core.dependencies import (
+    get_ai_client,
+    get_media_clients,
+    get_notification_service,
+    get_qb_client,
+    get_radarr_clients,
+    get_server_by_token,
+    get_sonarr_clients,
+    get_task_queue,
+    get_tmdb_client,
+    get_tvdb_client,
+)
+from models.emby_webhook import EmbyPayload, LibraryDeletedEvent, LibraryNewEvent
 from models.events import NotificationEvent
 from models.jellyfin_webhook import JellyfinPayload, NotificationType
 from models.orm import ServerInstance
-from models.radarr import (RadarrPayload, RadarrWebhookAddedPayload,
-                           RadarrWebhookDownloadPayload)
-from models.sonarr import (SonarrPayload, SonarrWebhookDownloadPayload,
-                           SonarrWebhookSeriesAddPayload)
+from models.radarr import (
+    RadarrPayload,
+    RadarrWebhookAddedPayload,
+    RadarrWebhookDownloadPayload,
+)
+from models.sonarr import (
+    SonarrPayload,
+    SonarrWebhookDownloadPayload,
+    SonarrWebhookSeriesAddPayload,
+)
 from services.media_service import MediaService
 from services.notification_service import NotificationService
-from workers.nfo_worker import (create_episode_nfo, create_movie_nfo,
-                                handle_movie_add_metadata,
-                                handle_series_add_metadata)
-from workers.translator_worker import (add_translate_media_item,
-                                       cancel_translate_media_item)
+from workers.nfo_worker import (
+    create_episode_nfo,
+    create_movie_nfo,
+    handle_movie_add_metadata,
+    handle_series_add_metadata,
+)
+from workers.translator_worker import (
+    add_translate_media_item,
+    cancel_translate_media_item,
+)
 
 router = APIRouter()
 settings = get_settings()
+
 
 @router.post("/webhook/sonarr", status_code=202)
 async def sonarr_webhook(
@@ -46,42 +63,54 @@ async def sonarr_webhook(
     tvdb_client: TvdbClient | None = Depends(get_tvdb_client),
     task_queue: asyncio.Queue | None = Depends(get_task_queue),
     qb_client: QbittorrentClient | None = Depends(get_qb_client),
-    notify_service: NotificationService = Depends(get_notification_service)
+    notify_service: NotificationService = Depends(get_notification_service),
 ) -> Response:
     """处理来自 Sonarr 的 Webhook"""
     client = sonarr_clients.get(server_instance.id)
     if not client:
         return Response(content="Webhook received (Client Not Found)", status_code=200)
 
-    logger.info("收到来自 {} 的 Webhook, 事件类型: {}", client.server_name, payload.eventType)
+    logger.info(
+        "收到来自 {} 的 Webhook, 事件类型: {}", client.server_name, payload.eventType
+    )
 
     if isinstance(payload, SonarrWebhookSeriesAddPayload):
         if mapped_path := client.to_local_path(payload.series.path):
             payload.series.path = mapped_path
         if tmdb_client:
-            asyncio.create_task(handle_series_add_metadata(client, payload, tmdb_client, tvdb_client))
+            asyncio.create_task(
+                handle_series_add_metadata(client, payload, tmdb_client, tvdb_client)
+            )
 
         if client.notify_topic_id:
             await notify_service.send_to_topic(
                 topic_id=client.notify_topic_id,
                 event_type=NotificationEvent.SONARR_SERIES_ADD,
                 server_name=client.server_name,
-                series=payload.series
+                series=payload.series,
             )
     elif isinstance(payload, SonarrWebhookDownloadPayload):
-        if payload.episodeFile and payload.episodeFile.path and 'VCB-Studio' not in payload.episodeFile.path:
+        if (
+            payload.episodeFile
+            and payload.episodeFile.path
+            and "VCB-Studio" not in payload.episodeFile.path
+        ):
             if mapped_path := client.to_local_path(payload.episodeFile.path):
                 payload.episodeFile.path = mapped_path
             if tmdb_client:
-                asyncio.create_task(create_episode_nfo(payload, tmdb_client, tvdb_client))
+                asyncio.create_task(
+                    create_episode_nfo(payload, tmdb_client, tvdb_client)
+                )
             if task_queue:
-            await task_queue.put(Path(payload.episodeFile.path))
+                await task_queue.put(Path(payload.episodeFile.path))
 
         if payload.downloadId and qb_client:
-            asyncio.create_task(qb_client.torrents_set_share_limits(
-                torrent_hash=payload.downloadId,
-                seeding_time_limit=settings.qbittorrent_torrent_limit
-            ))
+            asyncio.create_task(
+                qb_client.torrents_set_share_limits(
+                    torrent_hash=payload.downloadId,
+                    seeding_time_limit=settings.qbittorrent_torrent_limit,
+                )
+            )
 
         if client.notify_topic_id:
             await notify_service.send_to_topic(
@@ -93,10 +122,11 @@ async def sonarr_webhook(
                 episodeFile=payload.episodeFile,
                 isUpgrade=payload.isUpgrade,
                 release=payload.release,
-                customFormatInfo=payload.customFormatInfo
+                customFormatInfo=payload.customFormatInfo,
             )
 
     return Response(content="Webhook received", status_code=200)
+
 
 @router.post("/webhook/radarr", status_code=202)
 async def radarrarr_webhook(
@@ -106,30 +136,42 @@ async def radarrarr_webhook(
     tmdb_client: TmdbClient | None = Depends(get_tmdb_client),
     task_queue: asyncio.Queue | None = Depends(get_task_queue),
     qb_client: QbittorrentClient | None = Depends(get_qb_client),
-    notify_service: NotificationService = Depends(get_notification_service)
+    notify_service: NotificationService = Depends(get_notification_service),
 ) -> Response:
     """处理来自 Radarr 的 Webhook"""
     client = radarr_clients.get(server_instance.id)
     if not client:
         return Response(content="Webhook received (Client Not Found)", status_code=200)
 
-    logger.info("收到来自 {} 的 Webhook, 事件类型: {}", client.server_name, payload.eventType)
+    logger.info(
+        "收到来自 {} 的 Webhook, 事件类型: {}", client.server_name, payload.eventType
+    )
 
     if isinstance(payload, RadarrWebhookDownloadPayload):
-        if payload.movieFile and payload.movieFile.path and 'VCB-Studio' not in payload.movieFile.path:
+        if (
+            payload.movieFile
+            and payload.movieFile.path
+            and "VCB-Studio" not in payload.movieFile.path
+        ):
             if mapped_path := client.to_local_path(payload.movieFile.path):
                 payload.movieFile.path = mapped_path
             if task_queue:
-            await task_queue.put(Path(payload.movieFile.path))
+                await task_queue.put(Path(payload.movieFile.path))
 
         if local_folder_path := client.to_local_path(payload.movieFile.path):
-            asyncio.create_task(create_movie_nfo(Path(local_folder_path), payload.movie.tmdbId, tmdb_client))
+            asyncio.create_task(
+                create_movie_nfo(
+                    Path(local_folder_path), payload.movie.tmdbId, tmdb_client
+                )
+            )
 
         if payload.downloadId and qb_client:
-            asyncio.create_task(qb_client.torrents_set_share_limits(
-                torrent_hash=payload.downloadId,
-                seeding_time_limit=settings.qbittorrent_torrent_limit
-            ))
+            asyncio.create_task(
+                qb_client.torrents_set_share_limits(
+                    torrent_hash=payload.downloadId,
+                    seeding_time_limit=settings.qbittorrent_torrent_limit,
+                )
+            )
 
         if client.notify_topic_id:
             await notify_service.send_to_topic(
@@ -140,7 +182,7 @@ async def radarrarr_webhook(
                 movieFile=payload.movieFile,
                 isUpgrade=payload.isUpgrade,
                 release=payload.release,
-                customFormatInfo=payload.customFormatInfo
+                customFormatInfo=payload.customFormatInfo,
             )
     elif isinstance(payload, RadarrWebhookAddedPayload):
         if local_folder_path := client.to_local_path(payload.movie.folderPath):
@@ -151,10 +193,11 @@ async def radarrarr_webhook(
                 topic_id=client.notify_topic_id,
                 event_type=NotificationEvent.RADARR_MOVIE_ADD,
                 server_name=client.server_name,
-                movie=payload.movie
+                movie=payload.movie,
             )
 
     return Response(content="Webhook received", status_code=200)
+
 
 @router.post("/webhook/emby", status_code=202)
 async def emby_webhook(
@@ -162,14 +205,16 @@ async def emby_webhook(
     server_instance: ServerInstance = Depends(get_server_by_token),
     ai_client: AIClientWarper | None = Depends(get_ai_client),
     media_clients: dict[int, MediaService] = Depends(get_media_clients),
-    notify_service: NotificationService = Depends(get_notification_service)
+    notify_service: NotificationService = Depends(get_notification_service),
 ) -> Response:
     """处理来自 Emby 的 Webhook"""
     client = media_clients.get(server_instance.id)
     if not client:
         return Response(content="Webhook received (Client Not Found)", status_code=200)
 
-    logger.info("收到来自 {} 的 Webhook, 事件类型: {}", client.server_name, payload.event)
+    logger.info(
+        "收到来自 {} 的 Webhook, 事件类型: {}", client.server_name, payload.event
+    )
 
     if isinstance(payload, LibraryNewEvent):
         if ai_client:
@@ -180,7 +225,7 @@ async def emby_webhook(
                 topic_id=client.notify_topic_id,
                 event_type=NotificationEvent.EMBY_LIBRARY_NEW,
                 server_name=client.server_name,
-                item=payload.item
+                item=payload.item,
             )
     elif isinstance(payload, LibraryDeletedEvent):
         if ai_client:
@@ -188,20 +233,25 @@ async def emby_webhook(
 
     return Response(content="Webhook received", status_code=200)
 
+
 @router.post("/webhook/jellyfin", status_code=202)
 async def jellyfin(
     payload: JellyfinPayload,
     server_instance: ServerInstance = Depends(get_server_by_token),
     ai_client: AIClientWarper | None = Depends(get_ai_client),
     media_clients: dict[int, MediaService] = Depends(get_media_clients),
-    notify_service: NotificationService = Depends(get_notification_service)
+    notify_service: NotificationService = Depends(get_notification_service),
 ) -> Response:
     """处理来自 Jellyfin 的 Webhook"""
     client = media_clients.get(server_instance.id)
     if not client:
         return Response(content="Webhook received (Client Not Found)", status_code=200)
 
-    logger.info("收到来自 {} 的 Webhook, 事件类型: {}", client.server_name, payload.notification_type)
+    logger.info(
+        "收到来自 {} 的 Webhook, 事件类型: {}",
+        client.server_name,
+        payload.notification_type,
+    )
 
     if payload.notification_type == NotificationType.ITEM_ADDED:
         if ai_client:
@@ -212,7 +262,7 @@ async def jellyfin(
                 topic_id=client.notify_topic_id,
                 event_type=NotificationEvent.JELLYFIN_LIBRARY_NEW,
                 server_name=client.server_name,
-                item=payload.item_id
+                item=payload.item_id,
             )
     elif payload.notification_type == NotificationType.ITEM_DELETED:
         if ai_client:
